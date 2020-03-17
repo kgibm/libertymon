e1073926a7b408dd6f339f785cff68dfe470000d
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2020-03-17 15:33:25 -0700

    Add com.ibm.websphere.appserver.api.monitor dependency

--- src/main/java/com/example/liberty/LibertyMonInitializer.java
+++ src/main/java/com/example/liberty/LibertyMonInitializer.java
@@ -1,11 +1,17 @@
 package com.example.liberty;
 
+import java.lang.management.ManagementFactory;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.management.JMX;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+
+import com.ibm.websphere.monitor.jmx.ThreadPoolMXBean;
 
 @WebListener
 public final class LibertyMonInitializer implements ServletContextListener {
@@ -16,14 +22,34 @@ public final class LibertyMonInitializer implements ServletContextListener {
 	private static final Logger LOG = Logger.getLogger(SOURCE_CLASS);
 
 	static {
-		LibertyMon.systemPrint("Version: " + LibertyMon.VERSION);
+		LibertyMonUtilities.systemPrint("Version: " + LibertyMonUtilities.VERSION);
 	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		if (LOG.isLoggable(Level.FINER))
 			LOG.entering(SOURCE_CLASS, "contextInitialized");
-		
+
+		try {
+			MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
+
+			// https://www.ibm.com/support/knowledgecenter/SSEQTP_liberty/com.ibm.websphere.wlp.doc/ae/rwlp_mbeans_list.html
+			ObjectName objectName = new ObjectName("WebSphere:type=ThreadPoolStats,name=Default Executor");
+
+			if (mbeanServer.isRegistered(objectName)) {
+				
+				if (LOG.isLoggable(Level.INFO))
+					LOG.info("LibertyMon started");
+
+				ThreadPoolMXBean threadPool = JMX.newMBeanProxy(mbeanServer, objectName, ThreadPoolMXBean.class);
+
+				LibertyMonUtilities.systemPrint(threadPool.getActiveThreads());
+			} else {
+				LibertyMonUtilities.systemPrint("ThreadPoolStats is not registered");
+			}
+		} catch (Throwable t) {
+			LibertyMonUtilities.handleException(t, LOG, SOURCE_CLASS, "contextInitialized", "Failed to lookup MBeans");
+		}
 
 		if (LOG.isLoggable(Level.FINER))
 			LOG.exiting(SOURCE_CLASS, "contextInitialized");
@@ -34,7 +60,8 @@ public final class LibertyMonInitializer implements ServletContextListener {
 		if (LOG.isLoggable(Level.FINER))
 			LOG.entering(SOURCE_CLASS, "contextDestroyed");
 
-		LibertyMon.systemPrint("Application unloaded.");
+		if (LOG.isLoggable(Level.INFO))
+			LOG.info("Application unloaded.");
 
 		if (LOG.isLoggable(Level.FINER))
 			LOG.exiting(SOURCE_CLASS, "contextDestroyed");
--- src/main/java/com/example/liberty/LibertyMonUtilities.java
+++ src/main/java/com/example/liberty/LibertyMonUtilities.java
@@ -2,16 +2,21 @@ package com.example.liberty;
 
 import java.util.logging.Logger;
 
-public final class LibertyMon {
+public final class LibertyMonUtilities {
 	public static final String APPNAME = "LibertyMon";
 	public static final String VERSION = "0.1.20200317";
 
-	private static final String SOURCE_CLASS = LibertyMon.class.getName();
+	private static final String SOURCE_CLASS = LibertyMonUtilities.class.getName();
 	
 	@SuppressWarnings("unused")
 	private static final Logger LOG = Logger.getLogger(SOURCE_CLASS);
 
 	public static final void systemPrint(Object message) {
 		System.out.println(APPNAME + ": " + message);
+	}
+	
+	public static final void handleException(Throwable t, Logger log, String sourceClass, String sourceMethod, String message) {
+		systemPrint("ERROR: " + sourceClass + "." + sourceMethod + ": " + message);
+		t.printStackTrace();
 	}
 }
